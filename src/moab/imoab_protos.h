1464f6016bf51e35af00074520d8f6e1988bc2cf
Iulian Grindeanu <iulian@mcs.anl.gov>
2016-08-29 13:53:00 -0500

    use correct macro for fortran
    instead of always adding _ and converting to lowercase,
    use MOAB_FC_FUNC macro and IMOAB_FC_WRAPPER

--- src/moab/imoab_protos.h
+++ src/moab/imoab_protos.h
@@ -1,31 +1,45 @@
+#ifndef IMOAB_PROTOS_H
+#define IMOAB_PROTOS_H
 
-#define iMOAB_Initialize                     imoab_initialize_
-#define iMOAB_InitializeFortran              imoab_initializefortran_
-#define iMOAB_Finalize                       imoab_finalize_
-#define iMOAB_RegisterApplication            imoab_registerapplication_
-#define iMOAB_RegisterFortranApplication     imoab_registerfortranapplication_
-#define iMOAB_DeregisterApplication          imoab_deregisterapplication_
-#define iMOAB_ReadHeaderInfo                 imoab_readheaderinfo_
-#define iMOAB_LoadMesh                       imoab_loadmesh_
-#define iMOAB_WriteMesh                      imoab_writemesh_
-#define iMOAB_GetMeshInfo                    imoab_getmeshinfo_
-#define iMOAB_GetVertexID                    imoab_getvertexid_
-#define iMOAB_GetVertexOwnership             imoab_getvertexownership_
-#define iMOAB_GetVisibleVerticesCoordinates  imoab_getvisibleverticescoordinates_
-#define iMOAB_GetBlockID                     imoab_getblockid_
-#define iMOAB_GetBlockInfo                   imoab_getblockinfo_
-#define iMOAB_GetVisibleElementsInfo         imoab_getvisibleelementsinfo_
-#define iMOAB_GetBlockElementConnectivities  imoab_getblockelementconnectivities_
-#define iMOAB_GetElementConnectivity         imoab_getelementconnectivity_
-#define iMOAB_GetElementOwnership            imoab_getelementownership_
-#define iMOAB_GetElementID                   imoab_getelementid_
-#define iMOAB_GetPointerToSurfaceBC          imoab_getpointertosurfacebc_
-#define iMOAB_GetPointerToVertexBC           imoab_getpointertovertexbc_
-#define iMOAB_DefineTagStorage               imoab_definetagstorage_
-#define iMOAB_SetIntTagStorage               imoab_setinttagstorage_
-#define iMOAB_GetIntTagStorage               imoab_getinttagstorage_
-#define iMOAB_SetDoubleTagStorage            imoab_setdoubletagstorage_
-#define iMOAB_GetDoubleTagStorage            imoab_getdoubletagstorage_
-#define iMOAB_SynchronizeTags                imoab_synchronizetags_
-#define iMOAB_GetNeighborElements            imoab_getneighborelements_
-#define iMOAB_GetNeighborVertices            imoab_getneighborvertices_
+#include "moab/MOABConfig.h"
+
+#if defined(MOAB_FC_FUNC_)
+#define IMOAB_FC_WRAPPER MOAB_FC_FUNC_
+#elif defined(MOAB_FC_FUNC)
+#define IMOAB_FC_WRAPPER MOAB_FC_FUNC
+#else
+#define IMOAB_FC_WRAPPER(name,NAME) name
+#endif
+
+#define iMOAB_Initialize                     IMOAB_FC_WRAPPER( imoab_initialize, IMOAB_INITIALIZE )
+#define iMOAB_InitializeFortran              IMOAB_FC_WRAPPER( imoab_initializefortran, IMOAB_INITIALIZEFORTRAN )
+#define iMOAB_Finalize                       IMOAB_FC_WRAPPER( imoab_finalize, IMOAB_FINALIZE )
+#define iMOAB_RegisterApplication            IMOAB_FC_WRAPPER( imoab_registerapplication, IMOAB_REGISTERAPPLICATION )
+#define iMOAB_RegisterFortranApplication     IMOAB_FC_WRAPPER( imoab_registerfortranapplication, IMOAB_REGISTERFORTRANAPPLICATION )
+#define iMOAB_DeregisterApplication          IMOAB_FC_WRAPPER( imoab_deregisterapplication, IMOAB_DEREGISTERAPPLICATION )
+#define iMOAB_ReadHeaderInfo                 IMOAB_FC_WRAPPER( imoab_readheaderinfo, IMOAB_READHEADERINFO )
+#define iMOAB_LoadMesh                       IMOAB_FC_WRAPPER( imoab_loadmesh, IMOAB_LOADMESH )
+#define iMOAB_WriteMesh                      IMOAB_FC_WRAPPER( imoab_writemesh, IMOAB_WRITEMESH )
+#define iMOAB_GetMeshInfo                    IMOAB_FC_WRAPPER( imoab_getmeshinfo, IMOAB_GETMESHINFO )
+#define iMOAB_GetVertexID                    IMOAB_FC_WRAPPER( imoab_getvertexid, IMOAB_GETVERTEXID )
+#define iMOAB_GetVertexOwnership             IMOAB_FC_WRAPPER( imoab_getvertexownership, IMOAB_GETVERTEXOWNERSHIP )
+#define iMOAB_GetVisibleVerticesCoordinates  IMOAB_FC_WRAPPER( imoab_getvisibleverticescoordinates, IMOAB_GETVISIBLEVERTICESCOORDINATES )
+#define iMOAB_GetBlockID                     IMOAB_FC_WRAPPER( imoab_getblockid, IMOAB_GETBLOCKID )
+#define iMOAB_GetBlockInfo                   IMOAB_FC_WRAPPER( imoab_getblockinfo, IMOAB_GETBLOCKINFO )
+#define iMOAB_GetVisibleElementsInfo         IMOAB_FC_WRAPPER( imoab_getvisibleelementsinfo, IMOAB_GETVISIBLEELEMENTSINFO )
+#define iMOAB_GetBlockElementConnectivities  IMOAB_FC_WRAPPER( imoab_getblockelementconnectivities, IMOAB_GETBLOCKELEMENTCONNECTIVITIES )
+#define iMOAB_GetElementConnectivity         IMOAB_FC_WRAPPER( imoab_getelementconnectivity, IMOAB_GETELEMENTCONNECTIVITY )
+#define iMOAB_GetElementOwnership            IMOAB_FC_WRAPPER( imoab_getelementownership, IMOAB_GETELEMENTOWNERSHIP )
+#define iMOAB_GetElementID                   IMOAB_FC_WRAPPER( imoab_getelementid, IMOAB_GETELEMENTID )
+#define iMOAB_GetPointerToSurfaceBC          IMOAB_FC_WRAPPER( imoab_getpointertosurfacebc, IMOAB_GETPOINTERTOSURFACEBC )
+#define iMOAB_GetPointerToVertexBC           IMOAB_FC_WRAPPER( imoab_getpointertovertexbc, IMOAB_GETPOINTERTOVERTEXBC )
+#define iMOAB_DefineTagStorage               IMOAB_FC_WRAPPER( imoab_definetagstorage, IMOAB_DEFINETAGSTORAGE )
+#define iMOAB_SetIntTagStorage               IMOAB_FC_WRAPPER( imoab_setinttagstorage, IMOAB_SETINTTAGSTORAGE )
+#define iMOAB_GetIntTagStorage               IMOAB_FC_WRAPPER( imoab_getinttagstorage, IMOAB_GETINTTAGSTORAGE )
+#define iMOAB_SetDoubleTagStorage            IMOAB_FC_WRAPPER( imoab_setdoubletagstorage, IMOAB_SETDOUBLETAGSTORAGE )
+#define iMOAB_GetDoubleTagStorage            IMOAB_FC_WRAPPER( imoab_getdoubletagstorage, IMOAB_GETDOUBLETAGSTORAGE )
+#define iMOAB_SynchronizeTags                IMOAB_FC_WRAPPER( imoab_synchronizetags, IMOAB_SYNCHRONIZETAGS )
+#define iMOAB_GetNeighborElements            IMOAB_FC_WRAPPER( imoab_getneighborelements, IMOAB_GETNEIGHBORELEMENTS )
+#define iMOAB_GetNeighborVertices            IMOAB_FC_WRAPPER( imoab_getneighborvertices, IMOAB_GETNEIGHBORVERTICES )
+
+#endif /*IMOAB_PROTOS_H */
