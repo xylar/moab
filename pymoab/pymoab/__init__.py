3c5920219cd99369a954bab4184237ac2a552bd4
shriwise <pshriwise@gmail.com>
2017-03-13 16:27:13 -0500

    Adding general description of the pymoab package.

---
+++ pymoab/pymoab/__init__.py
@@ -0,0 +1,19 @@
+""" 
+Name
+====
+
+Description
+-----------
+
+PyMOAB: A Python interface to Argonne National Lab's Mesh Oriented dAtaBase
+(MOAB)
+
+PyMOAB provides a means of interactively interrogating, modifying, and generating MOAB mesh files.
+
+Much of the core functionality of MOAB has been implemented in the core.Core
+module. Interaction with this is intended to be largely analagous to interaction
+with MOAB via its native C++ API though some modifications have been made to
+allow for interaction with the interface using the various native Python
+constructs such as lists, tuples, etc.
+
+"""
