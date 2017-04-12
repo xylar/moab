6a2a3010de0278ff42966fe9ece2a6dd49566a62
Vijay Mahadevan <vijay.m@gmail.com>
2017-04-11 21:17:31 -0500

    Fixing some leftover issues with examples

--- examples/basic/UniformRefinement.cpp
+++ examples/basic/UniformRefinement.cpp
@@ -5,21 +5,17 @@
 
 using namespace moab;
 
-#define STRINGIFY_(X) #X
-#define STRINGIFY(X) STRINGIFY_(X)
-
 int main(int argc, char *argv[])
 {
-
   Core mb;
   Interface* mbImpl = &mb;
   ErrorCode error;
 
   if (argc==1)
-    {
-      std::cerr << "Usage: " << argv[0] << " [filename]" << std::endl;
-      return 1;
-    }
+  {
+    std::cerr << "Usage: " << argv[0] << " [filename]" << std::endl;
+    return 1;
+  }
 
   const char *filename = argv[1];
   error = mbImpl->load_file(filename); MB_CHK_ERR(error);
@@ -29,19 +25,24 @@ int main(int argc, char *argv[])
   // Usage: The level degrees array controls number of refinemetns and 
   // the degree of refinement at each level.
   // Example: int level_degrees[4] = {2,3,2,3};
-  int level_degrees[2] = {3,2};
-  int num_levels = sizeof(level_degrees) / sizeof(int);
+  std::vector<int> level_degrees;
+  if (argc > 2) {
+    level_degrees.resize(argc-2);
+    for (int i=2; i < argc; ++i) level_degrees[i-2] = atoi(argv[i]);
+  }
+  else {
+    level_degrees.resize(2);
+  }
+  int num_levels = static_cast<int>(level_degrees.size());
+
+  std::cout << "Starting hierarchy generation" << std::endl;
   std::vector<EntityHandle> set;
-
-  std::cout<<"Starting hierarchy generation"<<std::endl;
-
-  error = uref.generate_mesh_hierarchy(num_levels, level_degrees, set); MB_CHK_ERR(error);
-
-  std::cout<<"Finished hierarchy generation"<<std::endl;
+  error = uref.generate_mesh_hierarchy(num_levels, level_degrees.data(), set);MB_CHK_ERR(error);
+  std::cout << "Finished hierarchy generation" << std::endl;
 
   std::stringstream file;
-  file <<"mesh_hierarchy.h5m";
-  error = mbImpl->write_file(file.str().c_str()); MB_CHK_ERR(error);
+  file << "mesh_hierarchy.h5m";
+  error = mbImpl->write_file(file.str().c_str());MB_CHK_ERR(error);
   return 0;
 }
 
