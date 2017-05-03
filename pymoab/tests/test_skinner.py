796c6bbd43f492dc8fcf0683d294a23b42b69c36
Vijay Mahadevan <vijay.m@gmail.com>
2017-05-03 13:15:28 -0500

    Update the test_skinner test - verified

--- pymoab/tests/test_skinner.py
+++ pymoab/tests/test_skinner.py
@@ -1,10 +1,6 @@
-
 from ctypes import *
-lib1 = cdll.LoadLibrary('/usr/software/moab/dev/pymoab/lib/libMOAB.so')
 
-from pymoab import core
-from pymoab import types
-from pymoab import skinner
+from pymoab import core, types, skinner
 from driver import test_driver, CHECK_EQ, CHECK_NOT_EQ
 import numpy as np
 
@@ -12,15 +8,15 @@ import numpy as np
 def test_get_geometric_skin():
     mb = core.Core()
 
-    coords = np.array((0,0,0, 1,0,0, 2,0,0,
-                       0,1,0, 1,1,0, 2,1,0,
-                       0,2,0, 1,2,0, 2,2,0),dtype='float64')
+    coords = np.array([[0,0,0], [1,0,0], [2,0,0],
+                       [0,1,0], [1,1,0], [2,1,0],
+                       [0,2,0], [1,2,0], [2,2,0]],dtype='float64')
     verts = mb.create_vertices(coords)
     verts = np.array((verts[0:8]),dtype='uint64')
-    connect = np.array([[0, 1, 4, 3],
-                        [1, 2, 5, 4],
-                        [3, 4, 7, 6],
-                        [4, 5, 8, 7]],dtype='uint64')
+    connect = np.array([[1, 2, 5, 4],
+                        [2, 3, 6, 5],
+                        [4, 5, 8, 7],
+                        [5, 6, 9, 8]],dtype='uint64')
     quads = mb.create_elements(types.MBQUAD,connect)
 
     mskn = skinner.Skinner(mb)
@@ -35,25 +31,29 @@ def test_get_skin():
     mb = core.Core()
     rs = mb.get_root_set()
 
-    coords = np.array((0,0,0, 1,0,0, 2,0,0,
-                       0,1,0, 1,1,0, 2,1,0,
-                       0,2,0, 1,2,0, 2,2,0),dtype='float64')
+    coords = np.array([[0,0,0], [1,0,0], [2,0,0],
+                       [0,1,0], [1,1,0], [2,1,0],
+                       [0,2,0], [1,2,0], [2,2,0]],dtype='float64')
     verts = mb.create_vertices(coords)
     verts = np.array((verts[0:8]),dtype='uint64')
-    connect = np.array([[0, 1, 4, 3],
-                        [1, 2, 5, 4],
-                        [3, 4, 7, 6],
-                        [4, 5, 8, 7]],dtype='uint64')
+    connect = np.array([[1, 2, 5, 4],
+                        [2, 3, 6, 5],
+                        [4, 5, 8, 7],
+                        [5, 6, 9, 8]],dtype='uint64')
     quads = mb.create_elements(types.MBQUAD,connect)
 
     nelems = mb.get_entities_by_dimension(rs, 2)
     CHECK_EQ(len(nelems), 4)
 
+    mb.write_file('quads_test.vtk')
+
     mskn = skinner.Skinner(mb)
 
-    skin_verts = mskn.find_skin(rs, quads, False, False)
+    skin_verts = mskn.find_skin(rs, quads, True, False)
     CHECK_EQ(len(skin_verts), 8)
 
+    skin_edges = mskn.find_skin(rs, quads, False, False)
+    CHECK_EQ(len(skin_edges), 8)
 
 if __name__ == "__main__":
     tests = [
