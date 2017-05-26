bed4298d2040eafcae2ebc47fcbb12e1f11d906c
shriwise <pshriwise@gmail.com>
2017-05-26 09:46:20 -0500

    Small corrections to yt2moab.

--- examples/python/yt2moab.py
+++ examples/python/yt2moab.py
@@ -118,12 +118,14 @@ def yt2moab_uniform_gridgen(mb,ds):
     
         field_name = field[-1]
         data_arr = dd[field_name]
-    
-        #create field tag
-        density_tag = mb.tag_get_handle(field_name, 1, 2)
-    
-        #randomly tag :) 
-        mb.tag_set_data(density_tag, hex_handles, data_arr)
+
+        # only tags of length one
+        if data_arr.size == len(hex_handles):
+            #create field tag
+            density_tag = mb.tag_get_handle(field_name, 1, types.MB_TYPE_DOUBLE, True)
+            
+            # tag with data
+            mb.tag_set_data(density_tag, hex_handles, data_arr)
 
     #add created vertices and elements to meshset
     mb.add_entities(msh,vert_handles)
