866143f86aa413983911e74b656a35c277df44f0
shriwise <pshriwise@gmail.com>
2017-11-20 10:04:54 -0600

    Ensuring some tag convention values are the right type

--- pymoab/tests/test_tag.py
+++ pymoab/tests/test_tag.py
@@ -11,6 +11,24 @@ def test_tag_properties():
 
     CHECK_EQ(test_tag.get_length(), tag_size)
 
+def test_tag_conventions():
+
+    CHECK_EQ(type(types.MATERIAL_SET_TAG_NAME)   , str)
+    CHECK_EQ(type(types.DIRICHLET_SET_TAG_NAME)  , str)
+    CHECK_EQ(type(types.NEUMANN_SET_TAG_NAME)    , str)
+    CHECK_EQ(type(types.HAS_MID_NODES_TAG_NAME)  , str)
+    CHECK_EQ(type(types.GEOM_DIMENSION_TAG_NAME) , str)
+    CHECK_EQ(type(types.MESH_TRANSFORM_TAG_NAME) , str)
+    CHECK_EQ(type(types.GLOBAL_ID_TAG_NAME)      , str)
+    CHECK_EQ(type(types.CATEGORY_TAG_NAME)       , str)
+    CHECK_EQ(type(types.NAME_TAG_NAME)           , str)
+    
+    CHECK_EQ(type(types.CATEGORY_TAG_SIZE)       , int)    
+    CHECK_EQ(type(types.NAME_TAG_SIZE)           , int)
+
 if __name__ == "__main__":
-    tests = [test_tag_properties,]
+    tests = [test_tag_properties, test_tag_conventions]
     test_driver(tests)
+
+
+    
