ffd955f1563278526fbe5862edd05c9d532ee1d7
Patrick Shriwise <pshriwise@gmail.com>
2018-11-16 11:09:57 -0600

    Updating topo util tests

--- pymoab/tests/test_topo_util.py
+++ pymoab/tests/test_topo_util.py
@@ -2,7 +2,7 @@ from pymoab import core
 from pymoab import types
 from pymoab import topo_util
 from driver import test_driver
-from driver import CHECK_EQ
+from driver import CHECK_EQ, CHECK_NOT_EQ, CHECK_ITER_EQ
 import numpy as np
 
 
@@ -11,16 +11,18 @@ def test_get_bridge_adjacencies():
 
     coords = np.array((0,0,0,1,0,0,1,1,1),dtype='float64')
     verts = mb.create_vertices(coords)
-    verts = np.array(((verts[0],verts[1],verts[2]),),dtype='uint64')
-    tris = mb.create_elements(types.MBTRI,verts)
+    CHECK_EQ(len(verts), 3)
+    conn = np.array(((verts[0],verts[1],verts[2]),),dtype='uint64')
+    tris = mb.create_elements(types.MBTRI, conn)
 
     mtu = topo_util.MeshTopoUtil(mb)
 
     rs = mb.get_root_set()
     tris = mb.get_entities_by_dimension(rs, 2)
+    CHECK_EQ(len(tris), 1)
     adj_verts = mtu.get_bridge_adjacencies(tris, 0, 0, 1)
 
-    CHECK_EQ(adj_verts,verts)
+    CHECK_ITER_EQ(adj_verts,verts)
 
 
 def test_get_average_position():
@@ -56,7 +58,7 @@ def test_construct_aentities():
 
     edges_after = mb.get_entities_by_dimension(0, 1)
 
-    CHECK_EQ(edges_before, edges_after)
+    CHECK_NOT_EQ(edges_before, edges_after)
     CHECK_EQ(len(edges_after), 3)
 
 if __name__ == "__main__":
